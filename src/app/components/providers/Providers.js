1e4a5def7d883b078a9223447ea9f596c36d7840
EnzoWM02 <enzowmosman@gmail.com>
2024-09-14 16:10:33 -0300

    - WIP redux store

--- src/app/components/providers/Providers.js
+++ src/app/components/providers/Providers.js
@@ -5,16 +5,19 @@ import { NextUIProvider } from "@nextui-org/react";
 import { ToastContainer } from "react-toastify";
 import { SessionProvider } from "next-auth/react";
 import "react-toastify/dist/ReactToastify.css";
+import StoreProvider from "src/app/components/providers/StoreProvider";
 
 export function Providers({ session, children }) {
   return (
     <SessionProvider session={session}>
-      <NextUIProvider>
-        <I18nProvider locale="pt-BR">
-          <ToastContainer position="bottom-left" />
-          {children}
-        </I18nProvider>
-      </NextUIProvider>
+      <StoreProvider>
+        <NextUIProvider>
+          <I18nProvider locale="pt-BR">
+            <ToastContainer position="bottom-left" />
+            {children}
+          </I18nProvider>
+        </NextUIProvider>
+      </StoreProvider>
     </SessionProvider>
   );
 }
