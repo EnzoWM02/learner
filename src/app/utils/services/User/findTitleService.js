0d56215502582c3cb842b51b8bd45999675f4824
EnzoWM02 <enzowmosman@gmail.com>
2024-10-28 21:05:10 -0300

    - Fix user data bug

--- src/app/utils/services/User/findTitleService.js
+++ src/app/utils/services/User/findTitleService.js
@@ -1,5 +1,9 @@
 import TitleList from "src/app/(pages)/(protected)/store/constants/titles";
 
 export default function findTitleService(title) {
+  if (!title) {
+    return null;
+  }
+  
   return TitleList.find((listItem) => listItem.id === title.item_id);
 }
